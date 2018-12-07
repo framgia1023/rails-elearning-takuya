ebd412b33c771f7f51c00d0384e422edd14d83bc
Fujiwara Takuya <tky@FujiwaranoMacBook-puro.local>
2018-12-07 14:14:15 +0800

    add signin function & about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  resources :users
+  get '/login', to: 'sessions#new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
