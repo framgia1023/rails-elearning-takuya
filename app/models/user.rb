ef4a66a42d1dc8e66056dbe4a9309e79cfd7ede0
TakuyaFujiwara <44467330+TakuyaFujiwara@users.noreply.github.com>
2018-12-07 14:38:20 +0800

    add signin function & about page layout (#2)
    add signup function & about page layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
+
+  before_save {email.downcase!}
+  EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,   length: {maximum: 50},
+                                      format: {with: EMAIL_REGEX},
+                                      uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, length: { minimum: 6 }, allow_nil: true               
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_072408) do
+ActiveRecord::Schema.define(version: 2018_12_06_103504) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
